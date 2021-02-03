d4e20feb8f68c021ab0d3cdd064800f8047966cd
Penguin God <56172801+penguin-hacks@users.noreply.github.com>
2021-02-02 16:34:37 -0800

    added new to app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+  computer_player_turn();
   // The Listener -- these functions trigger when users do things
   $('.tic').click(function(e) {
     // This will run whenever anyone clicks on something with a 'tic' class.
@@ -8,7 +8,7 @@ $(document).ready(function() {
 
       // Computer does their turn
       computer_player_turn();
-      
+
       // Determine if someone wins
       check_for_winner();
       check_for_tie_game();
@@ -26,6 +26,15 @@ $(document).ready(function() {
 
 function computer_player_turn() {
   const available_tics = [];
+  const winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+  $.each(winning_combos, function(index, value){
+    let first = $('#'+value[0]).html();    // $('#3').html()
+    let second = $('#'+value[1]).html();    // $('#4').html()
+    let third = $('#'+value[2]).html();		 // $('#5').html()
+    if(first == second) {
+     
+    }
+  });
   $.each($('.tic'), function(index, value) {
       if($(value).html() == '#') {
           available_tics.push(value);
@@ -34,9 +43,6 @@ function computer_player_turn() {
 
   const computer_choice = Math.floor(Math.random() * available_tics.length);
   $(available_tics[computer_choice]).html('O');
-  console.log("computer_choice", computer_choice);
-  console.log("available_tics[computer_choice])", available_tics[computer_choice]);
-  console.log(available_tics);
 };
 
 function check_for_winner() {
@@ -52,10 +58,13 @@ function check_for_winner() {
     if(who_won == 'X') {
       alert("Congrats, You destroyed this completely random-moving computer!");
       $('.tic').html("#");
+      location.reload();
+      
     } else if(who_won == 'O') {
       prompt("You lost to a random computer, how do you feel?","Terrible");
       alert("noob");
       $('.tic').html("#");
+      location.reload();    
     }
   });
 }
@@ -71,7 +80,8 @@ function check_for_tie_game(){
   });
   
   if(is_tie) {
+    alert("it's a tie"); 
     $('.tic').html("#");
-    alert("it's a tie")
+    location.reload();
   }
 }
